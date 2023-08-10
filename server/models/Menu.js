5414c0cc9da9ee3caee336b12e699e6a6b4fcc5f
jbarz <jbarz0415@gmail.com>
2023-08-10 13:29:33 -0400

    seeds files, menu and user files

---
+++ server/models/Menu.js
@@ -0,0 +1,29 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Menu extends Model { }
+
+Menu.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        time_offered: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'menu',
+    }
+);
